bcf5b0768373beb4dca8581859671d0962b33f2f
Masayuki Maeda <mmae122@gmail.com>
2013-01-05 19:08:13 +0900

    The RSpec book Chapter8.2〜8.3辺り。Codebreakerの綴りがCode「B」reakerになっていたので、ネーミングエラーとなっていた。

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -1,26 +1,28 @@
-class Marker
-  def initialize(secret, guess)
-    @secret = secret
-    @guess = guess
-  end
-
-  def exact_match_count
-    (0..3).inject(0) do |count, index|
-      count + (exact_match?(@guess, index) ? 1 : 0)
+module Codebreaker
+  class Marker
+    def initialize(secret, guess)
+      @secret = secret
+      @guess = guess
     end
-  end
-
-  def number_match_count
-    (0..3).inject(0) do |count, index|
-      count + (number_match?(@guess, index) ? 1 : 0)
+  
+    def exact_match_count
+      (0..3).inject(0) do |count, index|
+        count + (exact_match?(@guess, index) ? 1 : 0)
+      end
     end
-  end
-
-  def exact_match?(guess, index)
-    @guess[index] == @secret[index]
-  end
-
-  def number_match?(guess, index)
-    @secret.include?(@guess[index]) && !exact_match?(@guess, index)
+  
+    def number_match_count
+      (0..3).inject(0) do |count, index|
+        count + (number_match?(@guess, index) ? 1 : 0)
+      end
+    end
+  
+    def exact_match?(index)
+      @guess[index] == @secret[index]
+    end
+  
+    def number_match?(index)
+      @secret.include?(@guess[index]) && !exact_match?(@guess, index)
+    end
   end
 end
